077e58882e87ff3e084e3a17fa6d2358310d66e2
Stormacq, Sebastien <stormacq@amazon.lu>
2019-06-22 23:11:08 +0200

    merge upstream exo player ICY

--- exoplayer2-ext-icy/src/main/java/saschpe/exoplayer2/ext/icy/IcyHttpDataSource.java
+++ exoplayer2-ext-icy/src/main/java/saschpe/exoplayer2/ext/icy/IcyHttpDataSource.java
@@ -1,7 +1,8 @@
 package saschpe.exoplayer2.ext.icy;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import android.util.Log;
 
 import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSource;
